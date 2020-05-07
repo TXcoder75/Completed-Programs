94adff6655e1f0d46aee71155ab32915684f2888
Kristopher <ktidwell76006@gmail.com>
2020-05-07 03:50:32 -0500

    Add files via upload
    A basic program I wrote checking the age of a person for a cruise vacation.

---
+++ vacationchecker.py
@@ -0,0 +1,8 @@
+
+age = int(input("Good morning.  How old are you?"))
+if age < 21:
+     print("come back when you're 21")
+elif age > 50:
+    print("Are you sure you want to come aboard grandpa?")
+else:
+    print("Welcome aboard, mate!")
